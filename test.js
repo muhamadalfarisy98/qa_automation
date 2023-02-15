a41bada559e5208dcf9a0f9de62ec4b9381f13ec
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2023-02-15 12:26:10 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const request = require("supertest")("https://reqres.in");
 const expect = require("chai").expect;
 
 describe("Post User reqres", function() {
-    it("Success register" , async function(){
+    it("Success register - 200" , async function(){
         const response = await request
         .post("/api/users")
         .send({name : "faris",
@@ -15,13 +15,12 @@ describe("Post User reqres", function() {
 
 
 const petstore = require("supertest")("https://dummyjson.com");
-const expect1 = require("chai").expect;
-describe("Get Pet ", function() {
-    it("Success 200" , async function(){
+describe("Get Product ", function() {
+    it("Success get data - 200" , async function(){
         const response = await petstore
         .get("/products/1");
 
-        expect1(response.body.title).to.eql("iPhone 9");
+        expect(response.body.title).to.eql("iPhone 9");
         expect(response.body.price).to.eql(549);
     })
 })
